d76797467fbc4e1256d17eb7af03668dd3857fcb
Iridium2345 <Iridium2345@gmail.com>
2025-06-23 17:12:00 +0800

    feature(*): first commit

---
+++ crates/mod-src/setting/scripts/test.lua
@@ -0,0 +1,5 @@
+function get_mod_info(body)
+    local mod_list = {}
+    table.insert(mod_list, {name = "test", version = "1.0.0", url = "https://example.com", date = "2021-01-01", author = "test"})
+    return mod_list
+end
